f85a183b33d3df18ccfcbe7b2c3af57c7f15176c
Zaidis <chewybinx@gmail.com>
2020-02-28 21:53:26 -0600

    Update button.cs

--- Nihle/Assets/Scripts/button.cs
+++ Nihle/Assets/Scripts/button.cs
@@ -5,17 +5,30 @@ using UnityEngine;
 public class button : MonoBehaviour
 {
     public GameObject platform;
-
+    public Animator animator;
     public bool isPlatformButton;
     public bool isWorldChangeButton;
+
+    private void Awake() {
+        animator = GetComponent<Animator>();
+    }
     private void OnCollisionEnter2D(Collision2D collision) {
         if(collision.gameObject.CompareTag("Player1") || collision.gameObject.CompareTag("Player2")){
+            collision.gameObject.transform.parent = gameObject.transform;
+            animator.SetBool("isPressed", true);
             if(isPlatformButton == true) {
                 platform.SetActive(true);
             }
             if(isWorldChangeButton == true) {
 
             }
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision) {
+        if(collision.gameObject.CompareTag("Player1") || collision.gameObject.CompareTag("Player2")){
+            animator.SetBool("isPressed", false);
+            collision.gameObject.transform.parent = null;
         }
     }
 }
