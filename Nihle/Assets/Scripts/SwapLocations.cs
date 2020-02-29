c60ede5a16bcb27502e339227db991b692ba614a
ZAG852 <36207290+ZAG852@users.noreply.github.com>
2020-02-28 18:30:22 -0600

    Update SwapLocations.cs

--- Nihle/Assets/Scripts/SwapLocations.cs
+++ Nihle/Assets/Scripts/SwapLocations.cs
@@ -2,17 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SwapLocations : MonoBehaviour
+namespace swapper
 {
-    // Start is called before the first frame update
-    void Start()
+    public class SwapLocations : MonoBehaviour
     {
-        
-    }
+        GameObject player1;
+        GameObject player2;
+        // Start is called before the first frame update
+        void Start()
+        {
+            if (player1 == null)
+                player1 = GameObject.FindGameObjectWithTag("Player1");
+            if (player2 == null)
+                player2 = GameObject.FindGameObjectWithTag("Player2");
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+        void switchSpots()
+        {
+            Vector2 locOfP1 = player1.transform.position;
+            Vector2 locOfP2 = player2.transform.position;
+            player1.transform.position = locOfP2;
+            player2.transform.position = locOfP1;
+        }
     }
 }
