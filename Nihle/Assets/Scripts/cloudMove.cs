ea2b5d014774370ffe7205d8143862efa5e0f755
ZAG852 <36207290+ZAG852@users.noreply.github.com>
2020-02-29 17:03:19 -0600

    fixed it

---
+++ Nihle/Assets/Scripts/cloudMove.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cloudMove : MonoBehaviour
+{
+    public float speed;
+    private float timer = 10;
+    private void Update() {
+        timer -= Time.deltaTime;
+        transform.Translate(Vector2.right * Time.deltaTime);
+
+        if(timer <= 0) {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Nihle/Assets/Scripts/cloudScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cloudScript : MonoBehaviour
+{
+    public float timer;
+    public GameObject cloud;
+
+    private void Update() {
+        timer -= Time.deltaTime;
+        if(timer <= 0) {
+            Instantiate(cloud, new Vector2(Random.Range(-0.5f, 0.5f), Random.Range(-5f, 5f)), Quaternion.identity);
+            timer = 6f;
+        }
+    }
+}
