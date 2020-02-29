9ab4c4967fccb5dcaa32587521743101fbb1f958
Zaidis <chewybinx@gmail.com>
2020-02-29 16:48:59 -0600

    ;iohukugljk,jhv

--- Nihle/Assets/Scripts/cloudScript.cs
+++ Nihle/Assets/Scripts/cloudScript.cs
@@ -8,8 +8,10 @@ public class cloudScript : MonoBehaviour
     public GameObject cloud;
 
     private void Update() {
+        timer -= Time.deltaTime;
         if(timer <= 0) {
             Instantiate(cloud, new Vector2(Random.Range(-0.5f, 0.5f), Random.Range(-5f, 5f)), Quaternion.identity);
+            timer = 6f;
         }
     }
 }
