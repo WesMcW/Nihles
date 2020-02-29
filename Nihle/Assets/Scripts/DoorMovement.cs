4f7f58a2f2f9a2d31d1650071582a30200175783
Leah <Lebrra@users.noreply.github.com>
2020-02-28 21:02:29 -0600

    stuff

---
+++ Nihle/Assets/Scripts/DoorMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorMovement : MonoBehaviour
+{
+    public GameObject otherDoor;
+    Animator myAnim, otherAnim;
+
+    public GameObject usedDoor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAnim = GetComponent<Animator>();
+        otherAnim = otherDoor.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void teleport()
+    {
+        usedDoor.transform.position = otherDoor.transform.position;
+    }
+}
