0827f6a2ff6c6ee49d3be9bc2a196aaa52ea00e0
ZAG852 <36207290+ZAG852@users.noreply.github.com>
2020-02-29 23:42:29 -0600

    Sound Stuff with the sound manager

---
+++ Nihle/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioClip[] songFiles;
+    bool isNext = false;
+    public static SoundManager instance;
+    public AudioClip[] otherSounds;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+            Destroy(this);
+
+        StartCoroutine(PlaySong());
+    }
+    void playAtLocSound(float x, float y, string sound)
+    {
+        GameObject tmp = new GameObject();
+        tmp.AddComponent<AudioSource>();
+        for (int i = 0; i < otherSounds.Length; i++)
+            if (otherSounds[i].name == sound)
+                tmp.GetComponent<AudioSource>().clip = otherSounds[i];
+        tmp.AddComponent<TemporarySoundObject>();
+        Instantiate(tmp, new Vector2(x, y), Quaternion.identity);
+    }
+    void playRandom()
+    {
+        StopCoroutine(PlaySong());
+        isNext = false;
+        StartCoroutine(PlaySong());
+    }
+    void playNext()
+    {
+        StopCoroutine(PlaySong());
+        isNext = true;
+        StartCoroutine(PlaySong());
+    }
+
+    private IEnumerator PlaySong(int i = -1)
+    {
+        if (i == -1)
+            i = Random.Range(0, songFiles.Length);
+        
+        yield return new WaitForSeconds(songFiles[i].length);
+        if(isNext)
+            i = (i + 1)%songFiles.Length;
+        else
+            i = Random.Range(0, songFiles.Length);
+    }
+}
---
+++ Nihle/Assets/Scripts/TemporarySoundObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TemporarySoundObject : MonoBehaviour
+{
+    float Timer = 360;
+    private void Start()
+    {
+        GetComponent<AudioSource>().loop = false;
+        GetComponent<AudioSource>().Play();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Timer -= Time.deltaTime;
+        if(Timer <=0)
+        {
+            Destroy(this);
+        }
+    }
+}
