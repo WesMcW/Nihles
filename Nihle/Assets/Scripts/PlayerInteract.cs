2ffda574f7466bfe69d934d8aa298a75d37b429c
WesMcW <wesmcwhorter@gmail.com>
2020-02-29 18:48:34 -0600

    collect fix

--- Nihle/Assets/Scripts/PlayerInteract.cs
+++ Nihle/Assets/Scripts/PlayerInteract.cs
@@ -11,11 +11,28 @@ public class PlayerInteract : MonoBehaviour
     public int totalCollect, score;
     public GameObject otherPlayer;
 
+    public float timeToPick = 0, maxTime;
+    public bool collected = false;
+
     // Start is called before the first frame update
     void Start()
     {
         if (playerNum == 1) interactButton = "PlayerOneInteract";
         else interactButton = "PlayerTwoInteract";
+        maxTime = 0.1f;
+    }
+
+    private void Update()
+    {
+        if (collected)
+        {
+            timeToPick -= Time.deltaTime;
+        }
+        if(timeToPick <= 0)
+        {
+            collected = false;
+            timeToPick = maxTime;
+        }
     }
 
     private void OnCollisionStay2D(Collision2D collision)
@@ -28,17 +45,19 @@ public class PlayerInteract : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.CompareTag("sadCollect") && playerNum == 1)
+        if(collision.gameObject.CompareTag("sadCollect") && playerNum == 1 && !collected)
         {
             sadCollect++;
             totalIncrease();
             Destroy(collision.gameObject);
+            collected = true;
         }
-        if(collision.gameObject.CompareTag("hapCollect") && playerNum == 2)
+        if(collision.gameObject.CompareTag("hapCollect") && playerNum == 2 && !collected)
         {
             hapCollect++;
             totalIncrease();
             Destroy(collision.gameObject);
+            collected = true;
         }
     }
 
