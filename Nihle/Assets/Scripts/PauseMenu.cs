93c8dc86800df3acba65d23ba5533e07212fb2ed
limitDak <roxasbboy@gmail.com>
2020-02-29 19:23:00 -0600

    button navs!

--- Nihle/Assets/Scripts/PauseMenu.cs
+++ Nihle/Assets/Scripts/PauseMenu.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour
 {
 
     public bool isPaused = false;
     public GameObject pauseScreen;
+    public Button[] pauseScreenBtns;
+
     void Update()
     {
         if (Input.GetButtonUp("Cancel") || Input.GetButtonUp("PlayerOnePause") || Input.GetButtonUp("PlayerTwoPause"))
@@ -35,5 +38,22 @@ public class PauseMenu : MonoBehaviour
     {
         pauseScreen.SetActive(true);
         Time.timeScale = 0f;
+        setButtonNavs();
+    }
+
+    void setButtonNavs()
+    {
+        Navigation replayNav = new Navigation();
+        replayNav.mode = Navigation.Mode.Explicit;
+        replayNav.selectOnDown = pauseScreenBtns[1];
+        pauseScreenBtns[0].navigation = replayNav;
+
+        Navigation menuNav = new Navigation();
+        menuNav.mode = Navigation.Mode.Explicit;
+        menuNav.selectOnUp = pauseScreenBtns[0];
+        pauseScreenBtns[1].navigation = menuNav;
+
+
+        GameObject.Find("EventSystem").GetComponent<UnityEngine.EventSystems.EventSystem>().SetSelectedGameObject(pauseScreenBtns[0].gameObject);
     }
 }
