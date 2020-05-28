d476e81ed13e667d30d118b1769d8444790da270
Zaidis <chewybinx@gmail.com>
2020-05-28 13:46:25 -0500

    Bonus 5 level complete
    Moving platform script changed slightly with a new bool. If you want a platform that only needs to push the player, turn the bool on. Otherwise do not worry about it.

--- Nihle/Assets/Scripts/movingPlatforms.cs
+++ Nihle/Assets/Scripts/movingPlatforms.cs
@@ -7,6 +7,9 @@ namespace mover {
         private GameObject platform;
         public float speed;
         public float timer = 0;
+        //Dictates whether or not the platform is meant to push the player only. Thus allowing the player to not become a child
+        //of the platform
+        public bool isPush;
         [Tooltip("These are the two points that the platform will move towards.")]
         [SerializeField] private GameObject pointOne, pointTwo;
         private void Awake() {
@@ -28,13 +31,17 @@ namespace mover {
         }
 
         private void OnCollisionEnter2D(Collision2D collision) {
-            if(collision.gameObject.CompareTag("Player1") || collision.gameObject.CompareTag("Player2")) {
-                collision.gameObject.transform.parent = gameObject.transform;
+            if (isPush == false) {
+                if (collision.gameObject.CompareTag("Player1") || collision.gameObject.CompareTag("Player2")) {
+                    collision.gameObject.transform.parent = gameObject.transform;
+                }
             }
         }
         private void OnCollisionExit2D(Collision2D collision) {
-            if (collision.gameObject.CompareTag("Player1") || collision.gameObject.CompareTag("Player2")) {
-                collision.gameObject.transform.parent = null;
+            if (isPush == false) {
+                if (collision.gameObject.CompareTag("Player1") || collision.gameObject.CompareTag("Player2")) {
+                    collision.gameObject.transform.parent = null;
+                }
             }
         }
     }
